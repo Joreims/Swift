646d02abf954a88ce51c463dc7b44087557b978d
Jorge García <111717486+Joreims@users.noreply.github.com>
2024-11-16 12:26:31 -0500

    Strings

---
+++ String.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+
+let username = "Chris"
+let likesYourPostMessage = "likes your post"
+//let finishedMessage = username + " " + likesYourPostMessage
+
+
+let firstName = "Jorge"
+let lastName = "Garcia"
+let fullname = firstName + " " + lastName
+print (fullname)
+let finishedMessage = fullname + " " + likesYourPostMessage
+print (finishedMessage)
+let city = "Paris"
+let welcomeString = "Hello " + fullname + ", welcome to " + city + "!"
+
+
+let favoriteFood = "Pizza"
+let reason = "tasty"
+let favoriteFoodReason = "I like \(favoriteFood), because it is \(reason)"
+
+
+print (favoriteFoodReason)
+let number = 100
+let adjective = "big"
+let noun = "car"
+let place = "store"
+let positiveEmotion = "happy"
+let negativeEmotion = "sad"
+let fillInBlankStory = "Today was a big day for \(fullname). They had finally saved up \(number) dollars and were going to buy a \(adjective) \(noun). They went to the \(noun) \(place) feeling very \(positiveEmotion). But then they felt \(negativeEmotion). They were all out of \(noun)!"
+print (fillInBlankStory)
+
