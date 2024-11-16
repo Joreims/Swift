a9a11f8e12085f0867f9b9f6b7ee0c0ad333d5e3
Jorge García <111717486+Joreims@users.noreply.github.com>
2024-11-16 10:08:20 -0500

    First Class

---
+++ Swift/s.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+// one minute video = 150mb
+
+// iphone  = 8gb
+// Storage Iphone Exercise
+let storageMaxIphone = 8
+
+let storageActualIphone = 8 - 3
+
+let sizeVideo = 150
+
+let convertGbToMb = storageActualIphone * 1024
+
+let numberOfVideos = convertGbToMb/sizeVideo
+
+print ("The # of videos is: ",numberOfVideos)
+// Fixing your morning
+// How many minutes each morning activity usually takes?
+
+
+let brushTeeth = 3
+
+let shower = 20
+
+let breakfast = 10
+
+
