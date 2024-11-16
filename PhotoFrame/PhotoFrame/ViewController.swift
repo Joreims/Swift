0e6b2575d553cfd98eb41c687ca6f2bc3293d798
Jorge García <111717486+Joreims@users.noreply.github.com>
2024-11-16 11:48:41 -0500

    PhotoFrame

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by student on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrameTests/PhotoFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameTests.swift
+//  PhotoFrameTests
+//
+//  Created by student on 11/16/24.
+//
+
+import Testing
+@testable import PhotoFrame
+
+struct PhotoFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
