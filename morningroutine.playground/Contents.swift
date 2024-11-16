e2a8cc44b9d83dca9b13f8648d1d386d859d151f
Jorge García <111717486+Joreims@users.noreply.github.com>
2024-11-16 10:24:13 -0500

    First Class

---
+++ morningroutine.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// var greeting = "Hello, playground"
+
+// Optimize your morning routine, how many minutes ech activity usually takes?
+
+let brushTeeth = 3
+
+let shower = 20
+
+let breakfast = 15
+
+let getDressed = 10
+
+let gym = 90
+
+let totalMinutes = brushTeeth + shower + breakfast + getDressed + gym
+
+print ("The total minutes of your morning routine is: \(totalMinutes)")
+
+let totalMinutesObligation = shower + brushTeeth + getDressed
+
+let totalMinutesEnjoy = breakfast + gym
+
+print ("The total minutes of your morning routine that you have to do by obligation is: \(totalMinutesObligation)")
+
+print("The total minutes of your morning routine that you enjoy to do is: \(totalMinutesEnjoy)")
