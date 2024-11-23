9e7e1e1d7faec7e2ee9c234ceb8b76c678aa1f0f
Jorge García <111717486+Joreims@users.noreply.github.com>
2024-11-23 12:25:45 -0500

    Class

---
+++ C-Parameters and Results/Parameters and Results.playground/Pages/Naming.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+
+
+func printHello(to: String) {
+    print ("Hello " + to)
+}
+printHello(to: "Maya")
+/*:
+ This function passes the side effect test and the function-as-a-sentence test.
+
+ - Experiment: Think of some more tasks a program might perform. Write them out as sentences, then think about how those sentences would look as functions.\
+ For example: “Get the first letter of ‘Swift’” would be `getTheFirstLetter(of: "Swift")`
+
+ But this new function has another problem. Learn what it is and how to fix it.
+ 
+[Previous](@previous)  |  page 11 of 18  |  [Next: Parameter Names and Argument Labels](@next)
+ */
